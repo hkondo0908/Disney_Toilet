58d4ffab6156148febf471cab514267eb06e0b74
近藤整 <hitoshi@kondouseinoAir.nrima1.kt.home.ne.jp>
2021-06-11 15:12:15 +0900

    first commit

---
+++ lib/disney_toilet.ex
@@ -0,0 +1,18 @@
+defmodule DisneyToilet do
+  @moduledoc """
+  Documentation for `DisneyToilet`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DisneyToilet.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/disney_toilet_test.exs
@@ -0,0 +1,8 @@
+defmodule DisneyToiletTest do
+  use ExUnit.Case
+  doctest DisneyToilet
+
+  test "greets the world" do
+    assert DisneyToilet.hello() == :world
+  end
+end
